d5ea779ac01afedd841e3905d017ec154a3312f9
milad heidari <milad.heidari.dev@gmail.com>
2022-12-10 10:11:10 +0330

    import BarChart component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import React from 'react';
 import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import BarChart from './components/charts/barChart/BarChart';
 function App() {
   return (
     <div className="App">
-      <BarChart/>
+
+        <CssBaseline />
+        <Container sx={{
+          height: '100vh',
+          width:'100vw',
+          padding:'4rem',
+}}>
+          <Box sx={{ 
+            bgcolor: '#2a2b3d',
+            padding:'1rem' ,
+            display:'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+            }} >
+            <BarChart/>
+
+          </Box>
+        </Container>
     </div>
+
   );
 }
 
