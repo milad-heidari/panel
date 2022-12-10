613a3ffc7b31f9d88efd8581a710583307ee4032
milad heidari <milad.heidari.dev@gmail.com>
2022-12-10 13:01:32 +0330

    add a div with chart class

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
     <div className="App">
             <div className='container'>
             <h3>Task one</h3>
+            <div className='chart'>
             <BarChart/>
+
+            </div>
             </div>
 
             <div className='container'>
