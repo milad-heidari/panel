80255eeff8e4946e1ab38b016ea2bb6a28549621
milad heidari <milad.heidari.dev@gmail.com>
2022-12-10 11:20:12 +0330

    import Table component to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import BarChart from './components/charts/barChart/BarChart';
 import Table from './components/table/Table';
 function App() {
   return (
     <div className="App">
             <BarChart/>
-            
             <Table/>
     </div>
 
