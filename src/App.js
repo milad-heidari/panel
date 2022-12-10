8d8e98b2e95827204673ca8369b220420b01391a
milad heidari <milad.heidari.dev@gmail.com>
2022-12-10 11:19:43 +0330

    import Table component to App.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import BarChart from './components/charts/barChart/BarChart';
+import Table from './components/table/Table';
 function App() {
   return (
     <div className="App">
-
-        <CssBaseline />
-        <Container sx={{
-          height: '100vh',
-          width:'100vw',
-          padding:'4rem',
-}}>
-          <Box sx={{ 
-            bgcolor: '#2a2b3d',
-            padding:'1rem' ,
-            display:'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
-            }} >
             <BarChart/>
-
-          </Box>
-        </Container>
+            
+            <Table/>
     </div>
 
   );
